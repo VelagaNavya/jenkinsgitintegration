129d84e3e148856bad50be06f14e263eb91b2467
Velaga Navya <velaganavya28@gmail.com>
2021-12-08 13:04:58 +0530

    add my Main.java

--- Main.java
+++ Main.java
@@ -1,8 +1,10 @@
-pubic class Main
-{
-   public static void main(String args[])
-     {
-    	 System.out.println("hello");
+public class Main {
 
-     }
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello world");
+
+	}
+
 }
